f7f4ea234c7bcf6999b42ed03a03aa332253e34d
Utkarsh1403 <utkarshsontakke123@gmail.com>
2022-03-24 17:01:29 +0530

    Recent updated

--- UnittestingExamples.py
+++ UnittestingExamples.py
@@ -2,6 +2,42 @@ import unittest
 
 def add(x,y):
     return x+y
+
+def checkDivisibleby7(x):
+
+    if x%7 == 0:
+        return True
+    else:
+        return False
+
+def cubevol(x):
+    return x*x*x
+
+
+
+class CheckVolume(unittest.TestCase):
+    def test_volume_cube(self):
+        x = 5.555
+        result = cubevol(x)
+        self.assertAlmostEqual(result,x*x*x)
+
+
+
+
+class CheckDivisibleby(unittest.TestCase):
+    def test_case_divisible(self):
+        x = 14
+        result = checkDivisibleby7(x)
+        self.assertTrue(result)
+
+
+    def test_case_divisible1(self):
+        x =9
+        result = checkDivisibleby7(x)
+        self.assertFalse(result)
+
+
+
 class myApp2(unittest.TestCase):
 
     def test_case1(self):
@@ -9,6 +45,7 @@ class myApp2(unittest.TestCase):
         b = 22
         result = add(a,b)
         self.assertEqual(a+b,result)
+
     def test_case2(self):
         a = 12.5
         b = 12.9
